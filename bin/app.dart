38c00d050f7f119db162faea1f4762af277a1279
Abu Nayem <abunayem118>
2023-08-04 21:45:30 +0600

    first commit

---
+++ bin/app.dart
@@ -0,0 +1,23 @@
+void main() {
+
+  //Declare two constant a and b
+  const a = 7;
+  const b = 3;
+
+  // Addition from a+b
+  print("Addition: $a + $b = ${a+b}");
+
+  // Subtraction from a-b
+  print("Subtraction: $a - $b = ${a-b}");
+
+  // Multiplication from a*b
+  print("Multiplication: $a * $b = ${a*b}");
+
+  // Division from a/b
+  print("Division: $a / $b = ${a/b}");
+
+  // Modulus from a%b
+  print("Modulus: $a % $b = ${a%b}");
+
+  
+}
